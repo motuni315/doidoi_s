f5faff1cac756e3c0e08e6aa69e4773aa4979091
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-09-12 10:36:53 +0900

    実行テスト

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GuraduationExamination.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GraduationExamination.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
